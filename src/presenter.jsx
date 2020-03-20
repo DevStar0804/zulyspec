601b6f893a320aec87d7f1d93ff599d407261563
DevStar0804 <kardzavaryan@gmail.com>
2020-03-20 15:58:07 -0300

    Huge lint cleanup, adherence to walmart-react style standards

--- src/presenter.jsx
+++ src/presenter.jsx
@@ -1,21 +1,22 @@
-import React from 'react/addons';
-import assign from 'object-assign';
-import cloneWithProps from 'react/lib/cloneWithProps';
-import Base from './base';
-import Radium from 'radium';
+/*global setInterval*/
 
-function startTime(date) {
-  var hours = date.getHours();
-  var minutes = date.getMinutes();
-  var seconds = date.getSeconds();
-  var ampm = hours >= 12 ? 'PM' : 'AM';
-  hours = hours % 12;
+import React from "react/addons";
+import cloneWithProps from "react/lib/cloneWithProps";
+import Base from "./base";
+import Radium from "radium";
+
+const startTime = function startTime(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let seconds = date.getSeconds();
+  const ampm = hours >= 12 ? "PM" : "AM";
+  hours %= 12;
   hours = hours ? hours : 12;
-  minutes = minutes < 10 ? '0'+minutes : minutes;
-  seconds = seconds < 10 ? '0'+seconds : seconds;
-  var strTime = hours + ':' + minutes + ':' + seconds + ' ' + ampm;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+  const strTime = hours + ":" + minutes + ":" + seconds + " " + ampm;
   return strTime;
-}
+};
 
 @Radium
 class Presenter extends Base {
@@ -23,20 +24,20 @@ class Presenter extends Base {
     super(props);
     this.state = {
       time: startTime(new Date())
-    }
+    };
   }
   _renderMainSlide() {
-    let child = this.props.slides[this.props.slide];
-    let presenterStyle = {
-      position: 'relative'
-    }
+    const child = this.props.slides[this.props.slide];
+    const presenterStyle = {
+      position: "relative"
+    };
     return cloneWithProps(child, {
       key: this.props.slide,
       slideIndex: this.props.slide,
       lastSlide: this.props.lastSlide,
       transition: [],
       transitionDuration: 0,
-      presenterStyle: presenterStyle
+      presenterStyle
     });
   }
   componentDidMount() {
@@ -47,113 +48,99 @@ class Presenter extends Base {
     }, 1000);
   }
   _renderNextSlide() {
-    let presenterStyle = {
-      position: 'relative'
-    }
-    let endStyle = {
-      position: 'absolute',
-      top: '50%',
-      left: '50%',
-      transform: 'translate(-50%, -50%)',
+    const presenterStyle = {
+      position: "relative"
+    };
+    const endStyle = {
+      position: "absolute",
+      top: "50%",
+      left: "50%",
+      transform: "translate(-50%, -50%)",
       margin: 0
-    }
-    let child = this.props.slides[parseInt(this.props.slide) + 1];
+    };
+    const child = this.props.slides[parseInt(this.props.slide) + 1];
     return child ? cloneWithProps(child, {
       key: this.props.slide + 1,
       slideIndex: this.props.slide + 1,
       lastSlide: this.props.lastSlide,
       transition: [],
       transitionDuration: 0,
-      presenterStyle: presenterStyle,
+      presenterStyle,
       appearOff: true
-    }) : <h1 style={[endStyle]}>END</h1>
-  }
-  _renderNotes() {
-    let child = this.props.slides[this.props.slide];
-    if (!child.props.notes) {return false;}
-    return <div dangerouslySetInnerHTML={{__html:child.props.notes}} />;
+    }) : <h1 style={[endStyle]}>END</h1>;
   }
   render() {
-    let styles = {
+    const styles = {
       presenter: {
-        height: '100%',
-        width: '100%',
-        display: 'flex',
+        height: "100%",
+        width: "100%",
+        display: "flex",
         flex: 1,
-        flexDirection: 'column'
+        flexDirection: "column"
       },
       header: {
-        position: 'absolute',
-        display: 'block',
-        color: 'white',
-        width: '100%',
-        height: '20%',
-        textAlign: 'center',
-        padding: '20px 50px',
+        position: "absolute",
+        display: "block",
+        color: "white",
+        width: "100%",
+        height: "20%",
+        textAlign: "center",
+        padding: "20px 50px"
       },
       slideInfo: {
-        position: 'relative',
-        top: '50%',
-        transform: 'translateY(-50%)',
-        float: 'left',
+        position: "relative",
+        top: "50%",
+        transform: "translateY(-50%)",
+        float: "left",
         margin: 0,
         lineHeight: 1,
-        display: 'inline-block',
+        display: "inline-block",
         fontSize: 28
       },
       clock: {
-        position: 'relative',
-        top: '50%',
-        transform: 'translateY(-50%)',
-        float: 'right',
+        position: "relative",
+        top: "50%",
+        transform: "translateY(-50%)",
+        float: "right",
         margin: 0,
         lineHeight: 1,
-        display: 'inline-block',
+        display: "inline-block",
         fontSize: 28
       },
       preview: {
-        display: 'flex',
-        width: '100%',
-        height: '100%',
-        alignItems: 'center',
-        justifyContent: 'center',
+        display: "flex",
+        width: "100%",
+        height: "100%",
+        alignItems: "center",
+        justifyContent: "center",
         flex: 1
       },
       main: {
-        display: 'inline-block',
-        width: '50%',
-        height: '60%',
-        border: '2px solid white',
+        display: "inline-block",
+        width: "50%",
+        height: "60%",
+        border: "2px solid white",
         padding: 20,
         margin: 20,
-        position: 'relative'
+        position: "relative"
       },
       next: {
-        display: 'inline-block',
-        width: '40%',
-        height: '50%',
-        border: '2px solid white',
+        display: "inline-block",
+        width: "40%",
+        height: "50%",
+        border: "2px solid white",
         padding: 20,
         margin: 20,
-        position: 'relative',
-        color: 'white'
-      },
-      notes: {
-        position: 'absolute',
-        display: 'block',
-        color: 'white',
-        width: '100%',
-        height: '20%',
-        bottom: '0px',
-        textAlign: 'left',
-        padding: '20px 50px',
-        columnCount: '2'
+        position: "relative",
+        color: "white"
       }
     };
     return (
-      <div className='spectacle-presenter' style={[styles.presenter]}>
+      <div className="spectacle-presenter" style={[styles.presenter]}>
         <div style={styles.header}>
-          <h2 style={styles.slideInfo}>Slide {this.props.slide + 1} of {this.props.slides.length}</h2>
+          <h2 style={styles.slideInfo}>
+            Slide {this.props.slide + 1} of {this.props.slides.length}
+          </h2>
           <h2 style={styles.clock}>{this.state.time}</h2>
         </div>
         <div style={styles.preview}>
@@ -164,11 +151,8 @@ class Presenter extends Base {
             {this._renderNextSlide()}
           </div>
         </div>
-        <div className="spectacle-presenter-notes" style={[styles.notes]}>
-          {this._renderNotes()}
-        </div>
       </div>
-    )
+    );
   }
 }
 
@@ -176,12 +160,12 @@ Presenter.propTypes = {
   lastSlide: React.PropTypes.number,
   slides: React.PropTypes.array,
   slide: React.PropTypes.number
-}
+};
 
 Presenter.contextTypes = {
   styles: React.PropTypes.object,
   flux: React.PropTypes.object,
   router: React.PropTypes.object
-}
+};
 
 export default Presenter;
