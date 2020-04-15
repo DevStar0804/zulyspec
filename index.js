5f12cc0143bfcc1a6af96793a7efe583e217646b
DevStar0804 <kardzavaryan@gmail.com>
2020-04-15 05:02:44 -0300

    Clear setInterval in cWU

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import MDXDocument from './test.mdx';
 
-/**
- * Experiment to test MDX -> JSX transpilation through babel.
- *
- * Outputs MDXDocument, changing MDXDocument will cause webpack
- * to hot-reload with new contents.
- */
+import Presentation from './example/src';
 
-render(<MDXDocument />, document.getElementById('root'));
+render(<Presentation />, document.getElementById('root'));
