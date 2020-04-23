f800c2a6939de9d7e9cf56ffb9c71b5ff081fdfa
DevStar0804 <kardzavaryan@gmail.com>
2020-04-23 20:11:07 -0300

    Update dependencies (#706)

--- src/components/progress.js
+++ src/components/progress.js
@@ -1,47 +1,185 @@
-import * as React from 'react';
-import propTypes from 'prop-types';
-import styled from 'styled-components';
-import { DeckContext } from '../hooks/use-deck';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import styled, { keyframes } from 'react-emotion';
 
-const Circle = styled('div')`
-  width: ${({ size }) => size}px;
-  height: ${({ size }) => size}px;
-  display: inline-block;
-  border: 1px solid ${({ color }) => color};
-  background: ${({ color, active }) => (active ? color : 'transparent')};
-  margin: ${({ size }) => size / 3}px;
-  border-radius: 50%;
-  pointer-events: all;
-  cursor: pointer;
+const Pacman = {
+  Base: styled.div(({ styles, position }) => [styles, position]),
+  Body: styled.div(props => props.styles)
+};
+
+// NOTE: rotateZ is 0.1 to generate two different animation names (emotion deduplication)
+const pacmanTopFrames = keyframes`
+  0% { transform: rotateZ(0.1deg) }
+  100% { transform: rotateZ(-30deg) }
 `;
 
-const Progress = props => {
-  const { numberOfSlides, state, goToSlide } = React.useContext(DeckContext);
-  return (
-    <div>
-      {Array(numberOfSlides)
-        .fill(0)
-        .map((_, idx) => (
-          <Circle
-            key={`progress-circle-${idx}`}
-            color={props.color}
-            active={state.currentSlide === idx}
-            size={props.size}
-            onClick={() => goToSlide(idx)}
+// NOTE: rotateZ is 0.1 to generate two different animation names (emotion deduplication)
+const pacmanBottomFrames = keyframes`
+  0% { transform: rotateZ(0.1deg) }
+  100% { transform: rotateZ(30deg) }
+`;
+
+const pacmanTopFramesBis = keyframes`
+  0% { transform: rotateZ(0deg) }
+  100% { transform: rotateZ(-30deg) }
+`;
+
+const pacmanBottomFramesBis = keyframes`
+  0% { transform: rotateZ(0deg) }
+  100% { transform: rotateZ(30deg) }
+`;
+
+const Point = styled.div(({ styles, position }) => [styles, position]);
+const Bar = styled.div(({ styles, width }) => [styles, width]);
+const Container = styled.div(props => props.styles);
+
+export default class Progress extends Component {
+  constructor() {
+    super(...arguments);
+    this.resolveProgressStyles = this.resolveProgressStyles.bind(this);
+  }
+
+  resolveProgressStyles(field) {
+    const { progressColor } = this.props;
+
+    if (!this.props.progressColor) {
+      return null;
+    }
+
+    const style = {};
+    let color;
+
+    if (!this.context.styles.colors.hasOwnProperty(progressColor)) {
+      color = progressColor;
+    } else {
+      color = this.context.styles.colors[progressColor];
+    }
+
+    style[field] = color;
+
+    return style;
+  }
+
+  getWidth() {
+    return {
+      width: `${(100 * this.props.currentSlideIndex) /
+        (this.props.items.length - 1)}%`
+    };
+  }
+
+  getPacmanStyle(side) {
+    const isBis = this.props.currentSlideIndex % 2 !== 0;
+    let animationName;
+
+    if (side === 'top') {
+      animationName = isBis ? pacmanTopFramesBis : pacmanTopFrames;
+    } else {
+      animationName = isBis ? pacmanBottomFramesBis : pacmanBottomFrames;
+    }
+
+    return {
+      animation: `${animationName} 0.12s linear 10 alternate both`
+    };
+  }
+
+  getPointPosition(i) {
+    return {
+      top: '-20px',
+      left: `${5 + 20 * (i - this.props.items.length / 2)}px`
+    };
+  }
+
+  getPointStyle(i) {
+    const style = this.getPointPosition(i);
+    if (this.props.currentSlideIndex >= i) {
+      style.opacity = 0;
+    }
+
+    return style;
+  }
+
+  render() {
+    const { type, currentSlideIndex, items } = this.props;
+    let style = this.context.styles.progress;
+    let markup;
+    switch (type) {
+      case 'none':
+        return false;
+      case 'pacman':
+        style = style.pacman;
+        markup = (
+          <div>
+            <Pacman.Base
+              styles={style.pacman}
+              position={this.getPointPosition(currentSlideIndex)}
+            >
+              <Pacman.Body
+                styles={[
+                  style.pacmanTop,
+                  this.getPacmanStyle('top'),
+                  this.resolveProgressStyles('background')
+                ]}
+              />
+              <Pacman.Body
+                styles={[
+                  style.pacmanBottom,
+                  this.getPacmanStyle('bottom'),
+                  this.resolveProgressStyles('background')
+                ]}
+              />
+            </Pacman.Base>
+            {items.map((item, i) => {
+              return (
+                <Point
+                  styles={[
+                    style.point,
+                    this.resolveProgressStyles('borderColor')
+                  ]}
+                  position={this.getPointStyle(i)}
+                  key={`presentation-progress-${i}`}
+                />
+              );
+            })}
+          </div>
+        );
+        break;
+      case 'number':
+        style = style.number;
+        markup = (
+          <div>
+            {currentSlideIndex + 1} / {items.length}
+          </div>
+        );
+        break;
+      case 'bar':
+        style = style.bar;
+        markup = (
+          <Bar
+            styles={[style.bar, this.resolveProgressStyles('background')]}
+            width={this.getWidth()}
           />
-        ))}
-    </div>
-  );
-};
+        );
+        break;
+      default:
+        return false;
+    }
+    return (
+      <Container
+        styles={[style.container, this.resolveProgressStyles('color')]}
+      >
+        {markup}
+      </Container>
+    );
+  }
+}
 
 Progress.propTypes = {
-  color: propTypes.string,
-  size: propTypes.number
+  currentSlideIndex: PropTypes.number,
+  items: PropTypes.array,
+  progressColor: PropTypes.string,
+  type: PropTypes.oneOf(['pacman', 'bar', 'number', 'none'])
 };
 
-Progress.defaultProps = {
-  color: '#fff',
-  size: 10
+Progress.contextTypes = {
+  styles: PropTypes.object
 };
-
-export default Progress;
--- webpack.config.js
+++ webpack.config.js
@@ -1,84 +1,52 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-/*
- * In order for the CLI to find loaders and babel configurations when
- * launching from another directory, we have to import the dependencies
- * explicitly from node_modules. Under the same circumstances, webpack
- * can not find the .babelrc file and so we must configure babel directly
- * in the webpack configuration.
- */
-const babelLoader = path.resolve(__dirname, 'node_modules', 'babel-loader');
-const presetEnv = path.resolve(__dirname, 'node_modules', '@babel/preset-env');
-const presetReact = path.resolve(
-  __dirname,
-  'node_modules',
-  '@babel/preset-react'
-);
-const pluginObjectSpread = path.resolve(
-  __dirname,
-  'node_modules',
-  '@babel/plugin-proposal-object-rest-spread'
-);
-const pluginClassProperties = path.resolve(
-  __dirname,
-  'node_modules',
-  '@babel/plugin-proposal-class-properties'
-);
-
-const mdxSlideLoader = path.resolve(__dirname, 'mdx-loader', 'index');
-
-const babelConfigOptions = {
-  babelrc: false,
-  presets: [require.resolve(presetEnv), require.resolve(presetReact)],
-  plugins: [
-    require.resolve(pluginObjectSpread),
-    require.resolve(pluginClassProperties)
-  ]
-};
+const webpack = require('webpack');
 
 module.exports = {
-  entry: './index.js',
+  mode: 'development',
+  devtool: 'cheap-module-source-map',
+  entry: './index',
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    publicPath: '/dist/'
   },
+  plugins: [
+    new webpack.NamedModulesPlugin(),
+    new webpack.NoEmitOnErrorsPlugin()
+  ],
   module: {
     rules: [
       {
-        test: /\.jsx?$/,
-        use: {
-          loader: require.resolve(babelLoader),
-          options: babelConfigOptions
-        }
+        test: /\.js$/,
+        include: ['index.js', 'src', 'example/assets', 'example/src'].map(
+          name => path.resolve(__dirname, name)
+        ),
+        loader: 'babel-loader'
       },
       {
-        test: /\.mdx?$/,
-        use: [
-          {
-            loader: require.resolve(babelLoader),
-            options: babelConfigOptions
-          },
-          { loader: require.resolve(mdxSlideLoader) }
-        ]
+        test: /\.css$/,
+        loader: 'style-loader!raw-loader'
       },
       {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: ['file-loader']
+        test: /\.svg$/,
+        include: path.join(__dirname, 'example/assets'),
+        loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
+      },
+      {
+        test: /\.png$/,
+        include: path.join(__dirname, 'example/assets'),
+        loader: 'url-loader?mimetype=image/png'
+      },
+      {
+        test: /\.jpg$/,
+        include: path.join(__dirname, 'example/assets'),
+        loader: 'url-loader?mimetype=image/jpg'
+      },
+      {
+        test: /\.gif$/,
+        include: path.join(__dirname, 'example/assets'),
+        loader: 'url-loader?mimetype=image/gif'
       }
     ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: `./index.html`
-    })
-  ],
-  resolve: {
-    alias: {
-      'spectacle-user-theme': path.resolve(
-        __dirname,
-        './src/theme/backup-user-theme'
-      )
-    }
   }
 };
