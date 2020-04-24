e9197429cc25318e5bf2914d95761bc5e5e4ae65
DevStar0804 <kardzavaryan@gmail.com>
2020-04-24 06:57:57 -0300

    Document creating slide templates (#733)

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-// START: test components to try rendering:
-import TestMDX from './examples/mdx/test-mdx';
-// import TestJs from './examples/js/test-js';
-// END: test components to try rendering
+import Presentation from './example/src';
 
-/**
- * Experiment to test MDX -> JSX transpilation through babel.
- *
- * Outputs MDXDocument, changing MDXDocument will cause webpack
- * to hot-reload with new contents.
- */
-
-render(<TestMDX />, document.getElementById('root'));
+render(<Presentation />, document.getElementById('root'));
--- src/components/Image.js
+++ src/components/Image.js
@@ -1,15 +1,53 @@
-import styled, { css } from 'styled-components';
-import { compose, layout, position } from 'styled-system';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { getStyles } from '../utils/base';
+import styled from 'react-emotion';
 
-const Image = styled('img')(
-  compose(
-    layout,
-    position
-  ),
-  css`
-    max-height: 100%;
-    max-width: 100%;
-  `
-);
+const StyledImg = styled.img(({ styles }) => [
+  styles.context,
+  styles.base,
+  {
+    maxWidth: '100%',
+    maxHeight: '100%'
+  },
+  styles.props,
+  styles.user
+]);
 
-export { Image };
+export default class Image extends Component {
+  render() {
+    const styles = {
+      context: this.context.styles.components.image,
+      base: getStyles.call(this),
+      props: {
+        width: this.props.width,
+        height: this.props.height,
+        display: this.props.display
+      },
+      user: this.props.style
+    };
+    return (
+      <StyledImg
+        className={this.props.className}
+        src={this.props.src}
+        alt={this.props.alt}
+        styles={styles}
+      />
+    );
+  }
+}
+
+Image.propTypes = {
+  alt: PropTypes.string,
+  className: PropTypes.string,
+  display: PropTypes.string,
+  height: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  src: PropTypes.string,
+  style: PropTypes.object,
+  width: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
+};
+
+Image.contextTypes = {
+  styles: PropTypes.object,
+  store: PropTypes.object
+};
